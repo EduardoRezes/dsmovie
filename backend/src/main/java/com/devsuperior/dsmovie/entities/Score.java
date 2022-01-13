418bcb6590134800bcc620f74e3d9ce7e9236912
Eduardo <contatoeduardorezes@gmail.com>
2022-01-13 10:52:14 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/Score.java
@@ -0,0 +1,41 @@
+package com.devsuperior.dsmovie.entities;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_score")
+public class Score {
+	
+	//Para chaves composta, deve-se instanciar para garantir que este objeto esteja instanciado.
+	@EmbeddedId
+	private ScorePK id = new ScorePK();
+	private Double value;
+	
+	public Score() {}
+
+	public ScorePK getId() {
+		return id;
+	}
+
+	public void setId(ScorePK id) {
+		this.id = id;
+	}
+
+	public Double getValue() {
+		return value;
+	}
+
+	public void setValue(Double value) {
+		this.value = value;
+	}
+	
+	public void setMovie(Movie movie) {
+		id.setMovie(movie);
+	}
+	
+	public void setUser(User user) {
+		id.setUser(user);
+	}
+}

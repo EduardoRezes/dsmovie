21835542b6f409b62a0dcd1b59862173fe9343ed
Eduardo <contatoeduardorezes@gmail.com>
2022-01-14 11:02:30 -0300

    Save score

---
+++ backend/src/main/java/com/devsuperior/dsmovie/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.devsuperior.dsmovie.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dsmovie.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+	//metodo de busca com mesmo nome do parametro recebido
+	User findByEmail(String email);
+}
